9fb44708ef300245c3b1e38cc15bcb4c3dd353f4
joaocrulhas <joao@isarlend.com>
2022-10-02 22:03:58 +0200

    AddNotification

--- src/cron/notification-job.ts
+++ src/cron/notification-job.ts
@@ -1,6 +1,8 @@
 import { TaskRepository } from "../repository/task-repository";
 import schedule from "node-schedule";
 import { EmailNotification } from "../services/notification/email-notification";
+import { Recurrence, Task } from "../api/entities/task.entity";
+import moment from "moment";
 
 schedule.scheduleJob("0 0 * * *", async function () {
   const taskRepository = new TaskRepository();
@@ -8,6 +10,32 @@ schedule.scheduleJob("0 0 * * *", async function () {
   await sendEmail();
 });
 
+const shouldNotifyTask = (task: Task): boolean => {
+  const { startDate, recurrence } = task;
+  const today = moment();
+
+  if (recurrence === 1) {
+    const diff = today.diff(moment(startDate), "days");
+    return diff % 15 === 0;
+  }
+
+  if (recurrence === 2) {
+    const diff = today.diff(moment(startDate), "month");
+    return diff === 1;
+  }
+
+  if (recurrence === 3) {
+    const diff = today.diff(moment(startDate), "quarter");
+    return diff === 1;
+  }
+
+  if (recurrence === 4) {
+    const diff = today.diff(moment(startDate), "years");
+    return diff === 1;
+  }
+
+  return false;
+};
 const sendEmail = async () => {
   const taskRepository = new TaskRepository();
   const tasks = await taskRepository.findAll({
@@ -16,6 +44,12 @@ const sendEmail = async () => {
   if (!tasks) return;
   const emailNotification = new EmailNotification();
   tasks.forEach(async (task) => {
-    await emailNotification.send(task.users, task);
+    if (shouldNotifyTask(task)) {
+      await emailNotification.send(task.users, task);
+    }
   });
 };
+
+sendEmail().then((data) => {
+  console.log(data);
+});
--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,27 +1,28 @@
 import { User, UserDTO } from "../../api/entities/user.entity";
 import { IService } from "../protocol";
-import {IRepository} from "../../repository/protocol";
-
+import { IRepository } from "../../repository/protocol";
 export class UserService implements IService<User, UserDTO> {
   constructor(private readonly userRepository: IRepository<UserDTO, User>) {}
   async add({ email, password, name }: UserDTO): Promise<User> {
-    return  await this.userRepository.add({ email, password, name })
+    return await this.userRepository.add({ email, password, name });
   }
 
-  async get(entityId?: number): Promise<User[] | null > {
+  async get(entityId?: number): Promise<User[] | null> {
     if (entityId) {
       const user = await this.userRepository.findOne(entityId);
-      if(!user) {
+      if (!user) {
         return null;
       }
-      return [{
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-        email: user.email,
-        name: user.name,
-        id: user.id,
-        tasks: user.tasks,
-      }];
+      return [
+        {
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+          email: user.email,
+          name: user.name,
+          id: user.id,
+          tasks: user.tasks,
+        },
+      ];
     }
     return await this.userRepository.findAll();
   }
